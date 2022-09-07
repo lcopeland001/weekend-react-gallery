8808052a8a9d620089c2d75c9eb1ff7ef09d6ac6
Latisha Copeland <lcopeland001@gmail.com>
2022-09-07 11:30:42 -0500

    add photos

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/281929933_1723346544714109_2676576352218401890_n.jpg', description: 'Photo of Jaylen.', likes: 0 },
+    { id: 1, path: 'images/258783886_1602005623514869_2660884853415203720_n.jpg', description: 'Photo of a Latisha.', likes: 0 },
+    { id: 1, path: 'images/280852741_1723346538047443_7185160376581087514_n.jpg', description: 'Photo of Tina.', likes: 0 },
+    { id: 1, path: 'images/274609988_1662517820796982_8258581033885268163_n', description: 'Tina and Davon', likes: 0 }
 ];
 
 module.exports = galleryItems;
